c372f640bbd6a5dde32b2032773205a1b8ec0a18
christopher-sousa <christopher.sousa@stage.consulting>
2023-10-29 20:01:36 -0300

    feat: initial sidebar

--- React/01-fundamentals-react/src/App.jsx
+++ React/01-fundamentals-react/src/App.jsx
@@ -1,18 +1,25 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
+import style from "./App.module.css"
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 function App() {
   return (
     <>
       <Header/>
-      <Post 
-        author="John"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dicta, dolorem a consequuntur praesentium, molestias distinctio tempore vero asperiores eveniet enim quisquam ad rerum quia. Sint fugiat quos quod consequuntur."
-      />
-      <Post 
-      author="Maira"
-      content="Novo post"
-    />
+      <div className={style.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post 
+            author="John"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dicta, dolorem a consequuntur praesentium, molestias distinctio tempore vero asperiores eveniet enim quisquam ad rerum quia. Sint fugiat quos quod consequuntur."
+          />
+          <Post 
+            author="Maira"
+            content="Novo post"
+          />
+        </main>
+      </div>
     </>
   )
 }
